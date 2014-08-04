4b237e906019359bbcf6d31905a31c8001ef4409
nero520 <1227372602@qq.com>
2014-08-04 21:33:21 +0800

    自动生成java代码

---
+++ zmparty/src/main/java/com/leisure/core/annotation/Title.java
@@ -0,0 +1,20 @@
+package com.leisure.core.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface Title {
+	/**
+	 * 
+	 * @return
+	 */
+	public String value() default "";
+}
